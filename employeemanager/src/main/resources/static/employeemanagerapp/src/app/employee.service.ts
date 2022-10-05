8abab291fab07692f4f0b2f5b4348de1f910b883
panvla <pandvlado@gmail.com>
2022-10-05 12:15:15 +0200

    added service methods . closes #7

--- employeemanager/src/main/resources/static/employeemanagerapp/src/app/employee.service.ts
+++ employeemanager/src/main/resources/static/employeemanagerapp/src/app/employee.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { Employee } from './employee';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,20 @@ export class EmployeeService {
 
   constructor(private http: HttpClient) { }
 
-  public getEmployees(): Observable<any>{
-    return this.http.get<any>(`${this.apiServerUrl}/employee/all`);
+  public getEmployees(): Observable<Employee[]>{
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employees`);
   }
+
+  public addEmployee(employee: Employee): Observable<Employee>{
+    return this.http.post<Employee>(`${this.apiServerUrl}`, employee);
+  }
+
+  public updateEmployee(employee: Employee): Observable<Employee>{
+    return this.http.put<Employee>(`${this.apiServerUrl}`,employee);
+  }
+
+  public deleteEmployee(employeeId: number): Observable<void>{
+    return this.http.delete<void>(`${this.apiServerUrl}/${employeeId}`);
+  }
+
 }
