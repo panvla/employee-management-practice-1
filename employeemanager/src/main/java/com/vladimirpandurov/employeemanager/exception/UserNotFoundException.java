c75c5d9a77821c55db22a1209265a16c3caebc33
panvla <pandvlado@gmail.com>
2022-10-05 10:22:35 +0200

    added jpa repository, service methods and exception for Optional. closes #4

---
+++ employeemanager/src/main/java/com/vladimirpandurov/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.vladimirpandurov.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
