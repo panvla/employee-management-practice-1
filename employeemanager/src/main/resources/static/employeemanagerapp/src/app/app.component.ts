fa2e18ea7ab5bb8302459349ee436ca2d00e380c
panvla <pandvlado@gmail.com>
2022-10-08 18:08:55 +0200

    delete functionality added and @Transactional on service class.closes #16

--- employeemanager/src/main/resources/static/employeemanagerapp/src/app/app.component.ts
+++ employeemanager/src/main/resources/static/employeemanagerapp/src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent implements OnInit {
   
   public employees: Employee[];
   public editEmployee: Employee;
+  public deleteEmployee: Employee;
 
   constructor(private employeeService: EmployeeService){}
 
@@ -58,6 +59,17 @@ export class AppComponent implements OnInit {
     );
   }
 
+  public onDeleteEmployee(employeeId: number): void {
+    this.employeeService.deleteEmployee(employeeId).subscribe(
+      (response: void)=>{
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    )
+  }
+
   public onOpenModal(employee: Employee , mode: string): void{
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
@@ -75,6 +87,7 @@ export class AppComponent implements OnInit {
     }
     
     if(mode === 'delete'){
+      this.deleteEmployee = employee;
       button.setAttribute('data-bs-target', '#deleteEmployeeModal');
     }
     container.appendChild(button);
